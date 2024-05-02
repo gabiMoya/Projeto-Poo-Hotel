abdf8a726a4340697e6324fc881afe10fcd23a92
gabiMoya <134560489+gabiMoya@users.noreply.github.com>
2024-05-02 20:41:32 -0300

    Add files via upload
    Exceptions do prof para ajustar o códig

---
+++ CpfGrdException.java
@@ -0,0 +1,9 @@
+public class CpfGrdException extends Exception{
+
+	public void impMsgCpfGrd(){
+System.out.println("\nCPF deve ser maior que Zero");
+	}
+	
+
+}
+
---
+++ CpfPeqException.java
@@ -0,0 +1,6 @@
+public class CpfPeqException extends Exception{
+
+	public void impMsgCpfPeq(){
+System.out.println("\nCPF deve ser maior que Zero");
+	}
+}
